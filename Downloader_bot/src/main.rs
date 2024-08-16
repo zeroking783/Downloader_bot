7be7aa5b4ccc47396c4d570b7b63a7b83261bbbe
zeroking783 <bakanov.vladimir2016@gmail.com>
2024-08-16 18:17:27 +0300

    The program connects to the database and adds the number of processor cores and time

--- Downloader_bot/src/main.rs
+++ Downloader_bot/src/main.rs
@@ -1,15 +1,35 @@
 mod connect_database;
 
 use connect_database::connect_database::{connect_to_database, disconnect_from_database};
-use tokio_postgres::Error;
+use tokio_postgres::{Client, NoTls, Error};
+use tokio::time::{interval, Duration};
+use sys_info;
+use chrono::{DateTime, Utc};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let client = connect_to_database().await?;
+    let cpu = sys_info::cpu_num().unwrap() as i32;
+    println!("Cpu num: {}", cpu);
 
-    // Выполните запрос или другую работу с клиентом здесь
+    let mut interval = interval(Duration::from_secs(10));
 
-    disconnect_from_database(&client).await;
+    loop {
+        interval.tick().await;
+        let client = connect_to_database().await?;
+
+        let now: DateTime<Utc> = Utc::now();
+        let now_str = now.to_rfc3339();
+
+        let insert = "INSERT INTO test_table (cpu_num, time_now) VALUES ($1, $2)";
+        client.execute(insert, &[&cpu, &now_str]).await?;
+
+        disconnect_from_database(&client).await;
+
+        println!("10 sec");
+    }
+
+
 
     Ok(())
+
 }
